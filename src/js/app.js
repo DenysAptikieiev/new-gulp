7f2455f578c3e42b81edb347fe020dc189d62d38
Denys Aptikieiev <denysaptikieiev@MacBook-Pro-Denys.local>
2023-12-30 23:50:24 -0600

    first commit termexim

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,47 @@
 import * as flsFunctions from './modules/functions.js';
-// import Swiper, { Navigation, Pagination } from 'swiper';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination ]);
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  allowTouchMove: true,
+  autoplay: {
+    delay: 1000,
+  },
+
+  breakpoints: {
+        768: {
+        slidesPerView: 1.5,
+        spaceBetween: 30,
+        allowTouchMove: false,
+        },
+    },
+
+  pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+  autoHeight: true,
+  mousewheel: {
+      invert: true,
+  },
+});
 
 flsFunctions.isWebP();
-console.log('Hello world!');
 
-// const swiper = new Swiper();
+const burger = document.querySelector(".burger");
+const navigation = document.querySelector(".navigation");
+
+burger.addEventListener("click", (e) => {
+    burger.classList.toggle("active");
+    navigation.classList.toggle("navigation_active");
+});
